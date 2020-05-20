d8ef3aa0dd3fb1c66950ee2d47875ff822f691c5
Hasan-93 <62036798+Hasan-93@users.noreply.github.com>
2020-05-20 04:55:45 -0700

    Update debug.py

--- Pull_Request/debug.py
+++ Pull_Request/debug.py
@@ -1,11 +1,14 @@
 def lone_sum(a, b, c):
-    if a >= b:
-        return c
-    elif a == c:
-        return b
-    elif b == c:
-        return a
-    elif a == b and a == c and b == c:
-        return 0
-    else:
-        return a+b+c
+
+  if a == b and a == c and b == c:
+      return 0
+  elif a == c:
+      return b
+  elif a >= b:
+      return c
+  elif b == c:
+      return a
+  else:
+      return a+b+c
+
+print(lone_sum(a, b, c))
